d2ab0a04346ccbfb4210036e758bd54cc860edfc
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-02-19 11:00:53 +0900

    modi

--- bm-live/frontend/vite.config.ts
+++ bm-live/frontend/vite.config.ts
@@ -1,20 +1,12 @@
-// vite.config.ts
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
-  resolve: {
-    extensions: [".tsx", ".ts", ".jsx", ".js", ".json"],
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      "/v1/api": {
-        target: "http://localhost:8080",
-        changeOrigin: true,
-      },
-    },
-  },
+export default defineConfig(({ mode }) => {
+  const base = mode === "admin" ? "/admin/" : "/";
+
+  return {
+    base,
+    plugins: [react(), tsconfigPaths()],
+  };
 });
