789252223ce2af0623c03b9792cf250dad6867f3
Hiroki-Shiraishi813 <shiraishi-h@groove-gear.co.jp>
2026-08-14 13:54:56 +0900

    modi

--- bm-live/frontend/src/pages/admin/master/CountryLeagueSeasonMasterReferencePage.tsx
+++ bm-live/frontend/src/pages/admin/master/CountryLeagueSeasonMasterReferencePage.tsx
@@ -290,6 +290,10 @@ function getTokyoTodayDateOnly(): string {
     year: "numeric",
     month: "2-digit",
     day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+    second: "2-digit",
+    hour12: false,
   }).format(new Date());
 }
 
